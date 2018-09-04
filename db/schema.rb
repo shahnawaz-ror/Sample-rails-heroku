7dbbbdc2efe111598bbec52878fc704fd5653dee
Mohammad shahnawaz <shahnawazkits@gmail.com>
2018-09-04 11:17:06 +0530

    Products reference added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_125016) do
+ActiveRecord::Schema.define(version: 2018_09_04_054459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_08_31_125016) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -63,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_125016) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
   add_foreign_key "students", "users"
   add_foreign_key "tasks", "products"
 end
